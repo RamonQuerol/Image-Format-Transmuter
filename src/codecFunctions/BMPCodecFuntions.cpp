dbef40ccae4194ba71fe3ad7ab141cb625108027
RamonQuerol <ramon.querol.leon@gmail.com>
2024-09-20 15:46:02 +0200

    BMP decoder structure changed to allow differents bit per pixels

--- src/codecFunctions/BMPCodecFuntions.cpp
+++ src/codecFunctions/BMPCodecFuntions.cpp
@@ -3,14 +3,40 @@
 #include "BMPCodecFunctions.h"
 #include <memory>
 
-Pixel decodePixelBMP(std::fstream & inputFile){
-    Pixel pixel;
-    pixel.blue = inputFile.get();
-    pixel.green = inputFile.get();
-    pixel.red = inputFile.get();
-    pixel.alpha = 255; // BMP as far as i know doesn't use alpha values, that will be translated as the maximun alpha value (fully opaque)
+std::unique_ptr<Pixel[]> decodeImgData24Bits(unsigned int width, unsigned int height, std::unique_ptr<unsigned char[]> & rawImageData){
 
-    return pixel;
+    std::unique_ptr<Pixel[]> imagePixels = std::make_unique<Pixel[]>(height*width);
+    
+    // When the row ends, the BMP format fills the space with 0s until the next multiple of 4
+    // So we have to skip those bytes to get to the next row.
+    int fillerPixels = width%4;
+
+    Pixel nPixel;
+    nPixel.alpha = 255; // 24 bits Bitmap don't store alpha bits, that will be translated as the maximun alpha value (fully opaque)
+
+    for(int i = 0; i<height; ++i){
+        // BMP stores the pixels in rows
+
+        // Although the compiler would probably do this optimization, its ok doing it for the sake of readability
+
+        // The offset that determines the row you are in, 
+        // we multiply width by 3 because each pixel ocupies 3 bytes in the binary.
+        int heightOffSet = i*(width*3+fillerPixels);
+
+        for(int j = 0; j<width; ++j){
+            nPixel.blue = rawImageData[heightOffSet + j*3];
+            nPixel.green = rawImageData[heightOffSet + j*3 + 1];
+            nPixel.red = rawImageData[heightOffSet + j*3 + 2];
+
+            // We use height-i-1 because the data is stored from botton to top, and the imageData
+            // in the Image struct should be stored from top to bottom.
+            imagePixels[(height-i-1)*width+j] = nPixel; 
+        }
+    }
+
+
+    return imagePixels;
+
 }
 
 int decodeBMP(std::fstream & inputFile, Image & decodedImage){
@@ -23,7 +49,10 @@ int decodeBMP(std::fstream & inputFile, Image & decodedImage){
     unsigned int compressionMethod, dataSize; 
     unsigned int verticalResolution, horizontalResolution, numOfColors, numOfImportantColors;
 
-    
+    std::unique_ptr<unsigned char[]> rawImageData; // Stores the bytes of the data block of the bitmap
+
+    std::unique_ptr<Pixel[]> imagePixels; // Stores the image data once parsed to Pixels
+
     //Image decodedImage;
 
     ///// FILE HEADER
@@ -61,21 +90,25 @@ int decodeBMP(std::fstream & inputFile, Image & decodedImage){
 
 
     inputFile.seekg(dataPos);
-
-    std::unique_ptr<Pixel[]> imageData = std::make_unique<Pixel[]>(height*width);
     
-    for(int i = 0; i<height; ++i){
-        // BMP stores the pixels in rows
+    ///// PIXEL DATA
 
-        for(int j = 0; j<width; ++j){
-            // We use height-i-1 because the data is stored from botton to top, and the imageData
-            // in the Image struct should be stored from top to bottom.
-            imageData[(height-i-1)*width+j] = decodePixelBMP(inputFile); 
-        }
-        
-        // When the row ends, the BMP format fills the space with 0s until the next multiple of 4
-        // So we have to skip those bytes to get to the next row.
-        inputFile.seekg(width%4, std::ios::cur);
+    rawImageData = std::make_unique<unsigned char[]>(dataSize);
+
+    // Extract the data from the file
+    for(int i = 0; i<dataSize; ++i){
+        rawImageData[i] = inputFile.get();
+    }
+
+
+    // Parse the data into the pixel format
+    switch(bitsPerPixel){
+        case 24:
+            imagePixels = decodeImgData24Bits(width, height, rawImageData);
+            break;
+        default:
+            std::cout << "The program has no support to bitMaps with " << bitsPerPixel << " bitsPerPixels.\n";
+            return -1;
     }
 
     // Small Test
@@ -85,7 +118,7 @@ int decodeBMP(std::fstream & inputFile, Image & decodedImage){
 
     decodedImage.heigth = height;
     decodedImage.width = width;
-    decodedImage.imageData = move(imageData);
+    decodedImage.imageData = move(imagePixels);
 
     return 0;
 }
